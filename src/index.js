dda6dbf41e1959223508be0d48ca3b25a6957678
Felcoj22 <est.juan.correa7@unimilitar.edu.co>
2021-11-22 21:58:40 -0500

    Agrega autenticacion de usuario

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { testingModules } from "./utils";
 import { getRings, getRings2, dbFirebase } from "../src/js/config/firebaseConfig"
 import { productCard } from "./js/components/productCard"
+import { autenticacion } from "./js/auth/autenticacion"
 
 
 

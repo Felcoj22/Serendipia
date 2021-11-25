01a8d5cf355d3a0c50bbc6e03ee3b1a53de0caf7
AndresFDaza <andresdaza04@hotmail.com>
2021-11-24 23:40:57 -0500

    auth de usuario

--- src/js/auth/autenticacion.js
+++ src/js/auth/autenticacion.js
@@ -1,1 +1,27 @@
-console.log('me importan')
+import { appFirebase } from '../config/firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth(appFirebase);
+
+const createNewUser = (authRef, correo, contrasena) => {
+  createUserWithEmailAndPassword(authRef, correo, contrasena)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log('userCreated:', user)
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('userNotCreated:', errorCode, '-->', errorMessage)
+      // ..
+    });
+}
+
+
+
+export { createNewUser, auth }
+
+
+export const Nombre = 'Andres'
